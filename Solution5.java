983a97819174deea9b60a23f1e0a26b624d672fa
sumanpathak14 <50736574+sumanpathak14@users.noreply.github.com>
2019-07-16 11:58:00 +0530

    Create Solution5.java

---
+++ Solution5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution5 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int I=scan.nextInt();
+		scan.close();
+		for(int j=1;j<=10;j++) {
+			System.out.println(I+"x"+j+"="+I*j);
+		}
+		
+	}
+
+}
