d955cb535c3dc2a1de5777cf9a49006176c81717
sumanpathak14 <50736574+sumanpathak14@users.noreply.github.com>
2019-07-18 12:26:40 +0530

    Create Solution16.java

---
+++ Solution16.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution16 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		try {
+		String S=sc.next();
+		System.out.println(Integer.parseInt(S));
+		}
+		catch(Exception e) {
+			System.out.println("Bad String");
+		}
+		sc.close();
+		
+	}
+
+}
